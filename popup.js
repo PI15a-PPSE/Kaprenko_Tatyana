c02175f704fe37e0bc584390a61884e5ffc9a860
Tatyana Karpenko <36289931+tttka98@users.noreply.github.com>
2018-03-19 00:14:59 +0300

    Add files via upload
    Создан файл-скрипт popup.js. Добавлены переменные для размера и адреса загружаемого изображения.

---
+++ popup.js
@@ -0,0 +1,13 @@
+/**
+* Адрес загружаемого изображения
+*
+* @var string $imageAddr
+*/
+var imageAddr = "http://www.kenrockwell.com/contax/images/g2/examples/31120037-5mb.jpg"; 
+
+/**
+* Точный размер загружаемого изображения
+*
+* @var int $downloadSize
+*/
+var downloadSize = 4995374; //bytes
